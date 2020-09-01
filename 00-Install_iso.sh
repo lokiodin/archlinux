6be9a537b9b7d8897007d6aaff218f31635f48ca
lokiodin <49552659+lokiodin@users.noreply.github.com>
2020-09-01 22:11:18 +0200

    Update 00-Install_iso.sh

--- 00-Install_iso.sh
+++ 00-Install_iso.sh
@@ -33,12 +33,12 @@ function echo_yellow(){
 
 
 function parted(){
-	echo_green "Partitioning of /dev/sda to sda1 (efi), sda2 (swap) and sda3 (ext4)"
+	# echo_green '"Partitioning of /dev/sda to sda1 (efi), sda2 (swap) and sda3 (ext4)"'
 	parted --script /dev/sda \
 		mklabel gpt \
 		mkpart part_efi fat32 1MiB 1GiB \
 		mkpart part_efi linux-swap 1GiB 2GiB \
-		mkpart part_efi ext4 2GiB 100% \
+		mkpart part_efi ext4 2GiB 100%
 }
 
 function format_part(){
@@ -56,3 +56,4 @@ echo_yellow " INSTALLATION OF THE BEGINING"
 echo_yellow "##############################"
 
 parted
+echo "FINININ"
