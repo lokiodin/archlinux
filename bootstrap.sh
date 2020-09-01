531ea24f6f283022cd63f8f7fa4e693feabac891
lokiodin <49552659+lokiodin@users.noreply.github.com>
2020-09-01 20:36:25 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -218,7 +218,7 @@ function install_niceties(){
 }
 
 function install_more_niceties(){
-	yay -Sy $YAY_INSTALL --noconfirm
+	sudo -u $NEW_USER bash -c 'yay -Sy $YAY_INSTALL --noconfirm'
 }
 
 if [ "$1" == "" ]
