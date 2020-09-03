a4c3a66e4ec64c7834f569875e12ffbe9deaae37
lokiodin <49552659+lokiodin@users.noreply.github.com>
2020-09-03 14:46:35 +0200

    Update full.sh

--- full.sh
+++ full.sh
@@ -5,7 +5,12 @@
 
 
 
-
+if [ "$1" == "" ]
+then
+	echo_red "Il faut renseigner un nom d'utilisateur."
+	echo "Usage: $0 <nouveau_utilisateur>"
+	exit
+fi
 
 
 ./scripts/05-install-grub.sh
@@ -15,5 +20,6 @@
 ./scripts/25-install-dependencies.sh
 ./scripts/30-create-config-new-user.sh
 ./scripts/35-install-yay-moreniceties-with-yay.sh
+
 
 echo "SCRIPT FINI"
