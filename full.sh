d39a7ff709ad5021e1bee3d04e6a87af4bda7678
lokiodin <49552659+lokiodin@users.noreply.github.com>
2020-09-08 14:59:50 +0200

    Update full.sh

--- full.sh
+++ full.sh
@@ -18,8 +18,8 @@ fi
 ./scripts/15-install-xorg.sh
 ./scripts/20-install-config-xfce.sh
 ./scripts/25-install-dependencies.sh
-./scripts/30-create-config-new-user.sh
-./scripts/35-install-yay-moreniceties-with-yay.sh
+./scripts/30-create-config-new-user.sh $1
+./scripts/35-install-yay-moreniceties-with-yay.sh $1
 
 
 echo "SCRIPT FINI"
