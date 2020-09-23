bb43ae5ae407796895e5ef1c95c91dc2ea77b243
lokiodin <49552659+lokiodin@users.noreply.github.com>
2020-09-23 22:42:20 +0200

    Update full.sh

--- full.sh
+++ full.sh
@@ -3,6 +3,28 @@
 # To install when you are on chroot (after partionning and configuring with iso)
 #
 
+# Set up de couleur à utiliser
+COLOR_RED=$(tput setaf 1)
+COLOR_GREEN=$(tput setaf 2)
+COLOR_YELLOW=$(tput setaf 3)
+COLOR_BLUE=$(tput setaf 4)
+COLOR_MAGENTA=$(tput setaf 5)
+COLOR_CYAN=$(tput setaf 6)
+COLOR_WHITE=$(tput setaf 7)
+BOLD=$(tput bold)
+COLOR_RESET=$(tput sgr0)
+
+function echo_red(){
+	echo "${COLOR_RED}${BOLD}$1${COLOR_RESET}"
+}
+
+function echo_green(){
+	echo "${COLOR_GREEN}${BOLD}$1${COLOR_RESET}"
+}
+
+function echo_yellow(){
+	echo "${COLOR_YELLOW}${BOLD}$1${COLOR_RESET}"
+}
 
 
 if [ "$1" == "" ]
